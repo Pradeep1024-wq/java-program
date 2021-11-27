8de7170db198b7a6c26e60d39994e8a4d3130c04
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-11-27 22:13:42 +0530

    Create PrintValue.java

---
+++ PrintValue.java
@@ -0,0 +1,12 @@
+public class PrintValue   
+{  
+public static void main(String[] args)   
+{  
+char z = 'a';  
+char x = 'b';  
+int PrintValue1 = z;  
+int PrintValue2 = x;  
+System.out.println("The Print value of " + z + " is: " + PrintValue1);  
+System.out.println("The Print value of " + x + " is: " + PrintValue2);  
+}  
+}
