d4e5b809956adc161177f1abe175302e82194398
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-11-27 22:00:23 +0530

    Create CreateObject.java

---
+++ CreateObject.java
@@ -0,0 +1,12 @@
+public class CreateObject   
+{    
+void show()    
+{    
+System.out.println("Welcome to PRADEEP GitHub");    
+}    
+public static void main(String[] args)   
+{       
+CreateObject obj = new CreateObject();    
+obj.show();    
+}    
+}
