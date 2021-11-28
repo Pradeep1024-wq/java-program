7392e72ee199d6b0b42a01e79c7003abe2fdc7b2
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-11-28 19:59:00 +0530

    Create AutomorphicNumber.java

---
+++ AutomorphicNumber.java
@@ -0,0 +1,24 @@
+public class AutomorphicNumber
+{      
+static boolean isAutomorphic(int num)   
+{     
+int square = num * num;   
+ 
+while (num > 0)   
+{    
+if (num % 10 != square % 10)    
+return false;   
+num = num/10;   
+square = square/10;   
+}   
+return true;   
+}    
+public static void main(String args[])   
+{   
+System.out.println(isAutomorphic(10) ? "Automorphic" : "Not Automorphic");   
+System.out.println(isAutomorphic(24) ? "Automorphic" : "Not Automorphic");
+System.out.println(isAutomorphic(1024) ? "Automorphic" : "Not Automorphic");     
+System.out.println(isAutomorphic(76) ? "Automorphic" : "Not Automorphic");   
+System.out.println(isAutomorphic(13) ? "Automorphic" : "Not Automorphic");   
+}   
+}
