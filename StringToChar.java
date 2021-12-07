987f12a393447e216434de18ad1fbc0d7349eff3
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-12-07 19:45:20 +0530

    Create StringToChar.java

---
+++ StringToChar.java
@@ -0,0 +1,18 @@
+public class StringToChar{  
+public static void main(String args[]){  
+String s="pradeep";  
+char z=s.charAt(0);
+char x=s.charAt(1);
+char c=s.charAt(2);
+char v=s.charAt(3);
+char b=s.charAt(4);
+char n=s.charAt(5);
+char m=s.charAt(6);
+System.out.println("1st character is: "+z);  
+System.out.println("2st character is: "+x);  
+System.out.println("3st character is: "+c);  
+System.out.println("4st character is: "+v);  
+System.out.println("5st character is: "+b);  
+System.out.println("6st character is: "+n);  
+System.out.println("7st character is: "+m);  
+}}  
