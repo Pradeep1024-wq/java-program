cfec99af08c446d4a1da013935a7ccaafd5660ab
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2021-12-10 21:44:28 +0530

    Create CharToInt.java

---
+++ CharToInt.java
@@ -0,0 +1,9 @@
+public class CharToInt{  
+public static void main(String args[]){  
+char c='N';  
+char c2='P';  
+int a=c;  
+int b=c2;  
+System.out.println(a);  
+System.out.println(b);  
+}}  
